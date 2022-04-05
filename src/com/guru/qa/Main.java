78583a952eb9a7f7df2aa2793cdaefbe52434dac
veronikapodgol <veronika.podgol@gmail.com>
2022-04-06 00:27:23 +0300

    change the Upper case

--- src/com/guru/qa/Main.java
+++ src/com/guru/qa/Main.java
@@ -1,8 +1,8 @@
-package com.guru.qa;
+package com.gu ru.qa;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello guru");
+        System.out.println("Hello Guru");
     }
 }
