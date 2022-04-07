4a47f3bf9194911fdc1f61a0d5513a1650275b4f
veronikapodgol <veronika.podgol@gmail.com>
2022-04-07 08:23:06 +0300

    switch to upper_case

--- src/com/guru/qa/Main.java
+++ src/com/guru/qa/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Guru");
         System.out.println("Hello Guru 3");
-        System.out.println("Hello Guru 4");
+        System.out.println("HELLO GURU 4");
     }
 }
